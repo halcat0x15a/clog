feb14faeec478f6e2978deffa7cc644164aa8308
Sanshiro Yoshida <halcat0x15a@gmail.com>
2014-11-09 06:49:51 +0900

    update cont version

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cont "0.3.2"]
+                 [cont "0.3.3"]
                  [unapply "0.2.0"]])
