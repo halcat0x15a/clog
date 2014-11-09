cf24b50041730d2b7dca0f7cda23437102c7fdbd
Sanshiro Yoshida <halcat0x15a@gmail.com>
2014-11-09 17:29:35 +0900

    add description

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject clog "0.1.3"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure Programming in Logic"
+  :url "https://github.com/halcat0x15a/clog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cont "0.3.3"]
-                 [unapply "0.2.0"]])
+                 [cont "0.3.3"]])
